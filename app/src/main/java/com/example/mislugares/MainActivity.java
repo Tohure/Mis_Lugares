3cc6371683a7d38547176f88c0c59f0becb0d4a5
Tohure <cr.htorres@gmail.com>
2016-05-20 00:30:40 -0500

    Temas parte 1

--- app/src/main/java/com/example/mislugares/MainActivity.java
+++ app/src/main/java/com/example/mislugares/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mislugares;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,24 +30,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    int drawable = android.R.drawable.stat_sys_upload;
 }
